5d3ded3348f7c4a3d5c3092dc1cf854f36a9d2e1
Rafinhaa <rafinhaa@me.com>
2023-06-26 19:15:32 -0300

    CSS-in-JS - Utilizando Styled Components

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,23 +1,14 @@
 import { StatusBar } from "expo-status-bar";
 import { FC } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Container, Title } from "./styles";
 
 const Groups: FC = () => {
   return (
-    <View style={styles.container}>
-      <Text>Groups</Text>
+    <Container>
+      <Title>Groups</Title>
       <StatusBar style="auto" />
-    </View>
+    </Container>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
 
 export default Groups;
