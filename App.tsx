583946f023b0a3aa2f937cb5beb5949fe53f5e2e
Rafinhaa <rafinhaa@me.com>
2023-06-26 19:56:03 -0300

    CSS-in-JS - Definindo o tema da aplicação

--- App.tsx
+++ App.tsx
@@ -1,8 +1,14 @@
 import { FC } from "react";
+import { ThemeProvider } from "styled-components";
+import darkTheme from "@theme/darkTheme";
 import { Groups } from "@screens/index";
 
 const App: FC = () => {
-  return <Groups />;
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <Groups />
+    </ThemeProvider>
+  );
 };
 
 export default App;
---
+++ src/theme/lightTheme.ts
@@ -0,0 +1,22 @@
+import { defaultTokens } from "./defaultTokens";
+
+export default {
+  COLORS: {
+    WHITE: "#121204",
+
+    GREEN_700: "#00875F",
+    GREEN_500: "#00B37E",
+
+    RED: "#F75A68",
+    RED_DARK: "#AA2834",
+
+    GRAY_700: "#F5F5F5",
+    GRAY_600: "#F0F0F0",
+    GRAY_500: "#EAEAEA",
+    GRAY_400: "#E0E0E0",
+    GRAY_300: "#BDBDBD",
+    GRAY_200: "#9E9E9E",
+    GRAY_100: "#757575",
+  },
+  ...defaultTokens,
+};
