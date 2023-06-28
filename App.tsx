552f9cbb0ea4afd9681ebac63c5ee03c141473a2
Rafinhaa <rafinhaa@me.com>
2023-06-28 19:33:29 -0300

    CSS-in-JS - Fontes personalizadas

--- App.tsx
+++ App.tsx
@@ -2,11 +2,31 @@ import { FC } from "react";
 import { ThemeProvider } from "styled-components/native";
 import themes from "@theme/index";
 import { Groups } from "@screens/index";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+import { ActivityIndicator } from "react-native";
 
 const App: FC = () => {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
+
+  const RenderApp: FC<{
+    fontsLoaded: boolean;
+  }> = ({ fontsLoaded }) =>
+    fontsLoaded ? (
+      <Groups />
+    ) : (
+      <ActivityIndicator size="large" style={{ flex: 1 }} />
+    );
+
   return (
     <ThemeProvider theme={themes.dark}>
-      <Groups />
+      <RenderApp fontsLoaded={fontsLoaded} />
     </ThemeProvider>
   );
 };
