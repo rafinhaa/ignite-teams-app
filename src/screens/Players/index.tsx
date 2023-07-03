29570693da64cfd0adc92931d0c4fe2296926e02
Rafinhaa <rafinhaa@me.com>
2023-07-03 19:05:42 -0300

    CSS-in-JS - Listando os filtros

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,5 +1,5 @@
-import { FC } from "react";
-import { Container, Form } from "./styles";
+import { FC, useState } from "react";
+import { Container, Form, HeaderList, NumberOfPlayers } from "./styles";
 import {
   ButtonIcon,
   Filter,
@@ -7,8 +7,12 @@ import {
   Highlight,
   Input,
 } from "@components/index";
+import { FlatList } from "react-native";
 
 const Groups: FC = () => {
+  const [team, setTeam] = useState("Time A");
+  const [players, setPlayers] = useState([]);
+
   return (
     <Container>
       <Header showBackButton />
@@ -20,7 +24,21 @@ const Groups: FC = () => {
         <Input placeholder="Nome da pessoa" autoCorrect={false} />
         <ButtonIcon icon="add" />
       </Form>
-      <Filter title="Time" />
+      <HeaderList>
+        <FlatList
+          horizontal
+          data={["Time A", "Time B"]}
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => (
+            <Filter
+              title={item}
+              isActive={item === team}
+              onPress={() => setTeam(item)}
+            />
+          )}
+        />
+        <NumberOfPlayers>{players.length}</NumberOfPlayers>
+      </HeaderList>
     </Container>
   );
 };
